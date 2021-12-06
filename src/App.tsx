776ebd24c43f8f92a7327262ec53bddf5fb95a3f
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-12-06 19:02:52 +0300

    feat: add header and body

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.scss';
 import { cn } from '@bem-react/classname';
+import Logo from './assets/logo/changellenge-logo-white.png';
 
 const cnApp = cn('App');
+const cnHeader = cn('Header');
+const cnBody = cn('Body');
 
 function App() {
   return (
     <div className={cnApp()}>
-
+      <div className={cnHeader()}>
+        <img className={cnHeader('Logo')} src={Logo} alt={'changellenge-logo-white'}/>
+      </div>
+      <div className={cnBody()}>
+        hi
+      </div>
     </div>
   );
 }
