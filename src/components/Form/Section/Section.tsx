263451d7dec32fcf315ac40776429021d6991dec
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-12-07 16:25:52 +0300

    feat: add switching buttons in section

--- src/components/Form/Section/Section.tsx
+++ src/components/Form/Section/Section.tsx
@@ -1,17 +1,24 @@
 import React, { FC } from "react";
 import { cn } from '@bem-react/classname';
+import Button from "../../styled-components/Button/Button";
 
 const cnSection = cn('Section');
 
 type PropsT = {
   data: string;
+  currentIndex: number;
+  maxIndex: number;
+  nextSection: () => void;
+  prevSection: () => void;
 }
 
-const Section: FC<PropsT> = ({data}) => {
+const Section: FC<PropsT> = ({data, nextSection, prevSection, maxIndex, currentIndex}) => {
 
   return (
     <div className={cnSection()}>
       <div>{data}</div>
+      <Button disabled={currentIndex === 0} label={"Назад"} onClick={prevSection}/>
+      <Button disabled={currentIndex >= maxIndex} label={"Вперед"} onClick={nextSection}/>
     </div>
   )
 }
