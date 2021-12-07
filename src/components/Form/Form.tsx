abd80c6e595b92678f6d74f18c180963e4f9ea00
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-12-07 13:38:28 +0300

    feat: add NavBar in Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { cn } from '@bem-react/classname';
+import './Form.scss';
+import NavBar from "./NavBar/NavBar";
 
+const cnForm = cn('Form');
+
+const testNames = ['Базовая информация', 'Образование', 'Опыт работы'];
+const sections = ["1", "2", "3"];
 const Form = () => {
 
+  const [activeSectionLabel, setActiveSectionLabel] = useState<string>(testNames[0]);
+
+  const nextSectionByName = (label: string) => {
+    setActiveSectionLabel(label);
+  }
 
   return (
-    <div>Hi</div>
+    <div className={cnForm()}>
+      <NavBar
+        sectionLabels={testNames}
+        activeSectionLabel={activeSectionLabel}
+        nextSectionByName={nextSectionByName}
+      />
+
+    </div>
   )
 }
 
