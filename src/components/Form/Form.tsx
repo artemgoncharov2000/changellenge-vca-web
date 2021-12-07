694e9141e1f620582c7865a641806d184f718af3
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-12-07 13:45:33 +0300

    feat: add section into form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { cn } from '@bem-react/classname';
 import './Form.scss';
 import NavBar from "./NavBar/NavBar";
+import map from 'lodash/map';
+import Section from "./Section/Section";
 
 const cnForm = cn('Form');
 
 const testNames = ['Базовая информация', 'Образование', 'Опыт работы'];
-const sections = ["1", "2", "3"];
+const sections = [{label: "Базовая информация"}, {label: "Образование"}, {label: "Опыт работы"}];
 const Form = () => {
 
   const [activeSectionLabel, setActiveSectionLabel] = useState<string>(testNames[0]);
@@ -22,7 +24,15 @@ const Form = () => {
         activeSectionLabel={activeSectionLabel}
         nextSectionByName={nextSectionByName}
       />
+      {
+        map(sections, section => {
+          if (section.label !== activeSectionLabel) {
+            return null;
+          }
 
+          return <Section data={section.label} />
+        })
+      }
     </div>
   )
 }
