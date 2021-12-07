dba651fb8a696d13f3dc26575554ada4d25dc7ff
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-12-07 16:46:20 +0300

    feat: add Navigation Block in Section

--- src/components/Form/Section/Section.tsx
+++ src/components/Form/Section/Section.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 import { cn } from '@bem-react/classname';
 import Button from "../../styled-components/Button/Button";
+import './Section.scss';
 
 const cnSection = cn('Section');
+const cnNavigationBlock = cn('NavigationBlock');
 
 type PropsT = {
   data: string;
@@ -17,8 +19,16 @@ const Section: FC<PropsT> = ({data, nextSection, prevSection, maxIndex, currentI
   return (
     <div className={cnSection()}>
       <div>{data}</div>
-      <Button disabled={currentIndex === 0} label={"Назад"} onClick={prevSection}/>
-      <Button disabled={currentIndex >= maxIndex} label={"Вперед"} onClick={nextSection}/>
+      <div className={cnNavigationBlock()}>
+        <Button disabled={currentIndex === 0} label={"Назад"} onClick={prevSection}/>
+        <Button
+          disabled={currentIndex >= maxIndex}
+          label={"Вперед"}
+          onClick={nextSection}
+          backgroundColor={"red"}
+          textColor={"white"}
+        />
+      </div>
     </div>
   )
 }
