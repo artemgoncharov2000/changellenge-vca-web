b1909db3de0278a01876cf2a57dbb644aab18366
adgoncharov <adgoncharov@yandex-team.ru>
2022-01-29 21:12:50 +0300

    feat: add get-initial-values.ts

---
+++ src/lib/get-initial-values.ts
@@ -0,0 +1,31 @@
+// TODO: прописать тип для data
+import forEach from "lodash/forEach";
+
+export const getInitialValues = (data: any) => {
+    const initialValues: any = {}
+    forEach(data, block => {
+        const { sections } = block;
+        forEach(sections, section => {
+            const { elements, id: sectionId, isArray } = section;
+            if (isArray) {
+                initialValues[sectionId] = [];
+            }
+            forEach(elements, element => {
+                const { id, type } = element;
+                if (isArray) {
+                    return;
+                }
+
+                if (type === 'INTERVAL_DATE_PICKER') {
+                    initialValues[String(id)] = {
+                        from: null,
+                        to: null,
+                    }
+                    return;
+                }
+                initialValues[String(id)] = '';
+            })
+        })
+    })
+    return initialValues;
+}
