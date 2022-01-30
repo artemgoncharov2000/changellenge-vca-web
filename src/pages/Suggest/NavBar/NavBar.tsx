386e7280d3ece1a480cbe7ddc9785f3780fc1bde
adgoncharov <adgoncharov@yandex-team.ru>
2022-01-30 16:10:46 +0300

    feat: create NavBar for suggest page

---
+++ src/pages/Suggest/NavBar/NavBar.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import './NavBar.scss';
+import map from 'lodash/map';
+import { cn } from '@bem-react/classname';
+
+type PropsT = {
+    activeSectionLabel: string;
+    sectionLabels: Array<string>;
+    nextSectionByName: (label: string) => void;
+}
+
+const NavBar: FC<PropsT> = ({activeSectionLabel, sectionLabels, nextSectionByName}) => {
+
+    const onClick = (label: string) => {
+        nextSectionByName(label);
+    }
+
+    return (
+        <div className={'suggest-page-nav-bar'}>
+            {
+                map(sectionLabels, (label, index) => (
+                    <div key={`${label}_${index}`} className={''}>
+                        <button
+                            type="button"
+                            className={'suggest-page-nav-bar__button'}
+                            onClick={() => onClick(label)}
+                        >
+                            {label}
+                        </button>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default NavBar;
