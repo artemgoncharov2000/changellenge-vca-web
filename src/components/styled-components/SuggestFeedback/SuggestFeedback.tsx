4b2567834834139a141739d47d38b84abc753d08
adgoncharov <adgoncharov@yandex-team.ru>
2022-01-31 11:17:09 +0300

    feat: add extra classes

--- src/components/styled-components/SuggestFeedback/SuggestFeedback.tsx
+++ src/components/styled-components/SuggestFeedback/SuggestFeedback.tsx
@@ -3,19 +3,29 @@ import EllipseIcon from '../../../assets/icons/ellipse-icon.svg';
 import './SuggestFeedback.scss'
 interface OwnProps {
     suggestId?: string; // TODO: Сделать обязательным полем
+    extraClassname?: string;
+    extraTitleClassname?: string;
+    extraButtonClassname?: string;
+    extraImageClassname?: string;
 }
 
 type Props = OwnProps;
 
 const SuggestFeedback: FunctionComponent<Props> = (props) => {
+    const {
+        extraClassname,
+        extraTitleClassname,
+        extraButtonClassname,
+        extraImageClassname,
+    } = props;
   // TODO Добавить действия при нажатии на Да или Нет
   return (
-      <div className={'suggest-feedback'}>
-          <p className={'suggest-feedback__title'}>{'Была ли данная рекомендация полезна для Вас?'}</p>
+      <div className={`suggest-feedback ${extraClassname}`}>
+          <p className={`suggest-feedback__title ${extraTitleClassname}`}>{'Была ли данная рекомендация полезна для Вас?'}</p>
           <div className={'suggest-feedback-action-block'}>
-              <button className={'suggest-feedback-action-block__button'} type="button">{'Да'}</button>
-              <img className={'suggest-feedback-action-block__image'} src={EllipseIcon} alt={'ellipse-icon'}/>
-              <button className={'suggest-feedback-action-block__button'} type="button">{'Нет'}</button>
+              <button className={`suggest-feedback-action-block__button ${extraButtonClassname}`} type="button">{'Да'}</button>
+              <img className={`suggest-feedback-action-block__image ${extraImageClassname}`} src={EllipseIcon} alt={'ellipse-icon'}/>
+              <button className={`suggest-feedback-action-block__button ${extraButtonClassname}`} type="button">{'Нет'}</button>
           </div>
       </div>
   );
